417b2d2349d75b7d9033a0164c22be1ad4912cfe
Nate <boo0330@gmail.com>
2016-08-25 03:59:36 +0000

    cell phone responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,8 @@
 //= require angular-animate
 //= require angular-ui-router
 //= require angular-rails-templates
+//= require angular-scroll/angular-scroll.js
+//= require ng-parallax/angular-parallax.js
 //= require masonry/dist/masonry.pkgd.js
 //= require oclazyload/dist/ocLazyLoad.js
 //= require jquery.fitvids/jquery.fitvids.js
