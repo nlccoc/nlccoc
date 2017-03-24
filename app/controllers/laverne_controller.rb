df55d075e64b94dc91384ad3ebbb093c64bbeeaf
Elvis Lee <elvislee0725@gmail.com>
2017-03-24 01:08:58 +0000

    Fix the code for inserting images

--- app/controllers/laverne_controller.rb
+++ app/controllers/laverne_controller.rb
@@ -1,8 +1,5 @@
 class LaverneController < ApplicationController
     def index
-<<<<<<< HEAD
-=======
       @mvideos_lv = Mvideo.where(location_id: 2).order(date: :desc)
->>>>>>> a404e15b0c87fe70e825ae6a87290e7b142fd6d4
     end  
 end
