c93353931905790ce60205852612949510f663c2
Nate <boo0330@gmail.com>
2017-04-17 02:08:54 +0000

    bug and production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,13 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
+    domain: "gmail.com",
+    #domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    user_name: "boo0330@gmail.com",
+    password: "soas1979"
+    #user_name: ENV["GMAIL_USERNAME"],
+    #password: ENV["GMAIL_PASSWORD"]
   }
 end
