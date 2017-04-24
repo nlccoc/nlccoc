ccc848113595132789c041618227ce3a22126b79
Elvis Lee <elvislee0725@gmail.com>
2017-04-24 07:04:34 +0000

    Limit 4 videos in La Verne homepage

--- app/controllers/laverne_controller.rb
+++ app/controllers/laverne_controller.rb
@@ -1,6 +1,6 @@
 class LaverneController < ApplicationController
     def index
       @header_bg='transparent'
-      @mvideos_lv = Mvideo.where(location_id: 2).order(date: :desc)
+      @mvideos_lv = Mvideo.where(location_id: 2).order(date: :desc).limit(4)
     end  
 end
