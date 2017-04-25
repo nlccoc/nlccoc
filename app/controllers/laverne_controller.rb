f4fac5b04060774dc3b2043eadfc8298df67248d
Elvis Lee <elvislee0725@gmail.com>
2017-04-25 23:27:34 +0000

    Add new videos_lv page for all La Verne sermon videos

--- app/controllers/laverne_controller.rb
+++ app/controllers/laverne_controller.rb
@@ -3,4 +3,8 @@ class LaverneController < ApplicationController
       @header_bg='transparent'
       @mvideos_lv = Mvideo.where(location_id: 2).order(date: :desc).limit(4)
     end  
+    
+    def videos
+      @mvideos_lv_all = Mvideo.where(location_id: 2).order(date: :desc)
+    end
 end
