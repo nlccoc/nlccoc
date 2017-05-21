cb545c62c6e631f5b8f5cca48f67f34cd1e4491f
Nate <boo0330@gmail.com>
2017-05-21 15:49:42 +0000

    log max

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,7 +1,7 @@
 class LogsController < ApplicationController
   before_action :authenticate_mgmt!
   def index
-    @logs = Log.all.order('datetime DESC').limit(100)
+    @logs = Log.all.order('datetime DESC').limit(200)
     
     respond_to do |format|
       format.html
--- app/models/mgmt.rb
+++ app/models/mgmt.rb
@@ -2,7 +2,7 @@ class Mgmt < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :timeout_in => 15.minutes
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :timeout_in => 90.minutes
   #attr_accessible :last_sign_in_at
   
   belongs_to :role
